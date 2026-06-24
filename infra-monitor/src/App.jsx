58a49ce1c6ea3aad8f03f1236cab7da0db4505a3
Christina Ann Pinto <SITI1615@stellar.com>
2026-06-24 18:46:57 +0530

    updating latest changes

--- infra-monitor/src/App.jsx
+++ infra-monitor/src/App.jsx
@@ -4,17 +4,17 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './pages/navbar/Navbar'
-import Dashboard from './pages/Dashboard/Dasboard'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <Navbar/>
-      <Dashboard/>
-    </>
-  )
+return (
+  <>
+    <Navbar />
+    <Dashboard />
+  </>
+)
 }
 
 export default App
--- infra-monitor/src/pages/Dashboard/Dashboard.jsx
+++ infra-monitor/src/pages/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dasboard</div>
+    <div>Dashboard</div>
   )
 }
 
